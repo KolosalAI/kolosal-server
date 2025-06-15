26a7947b22c7930ecfcf774e416f56882199a191
Rifky Bujana Bisri <rifkybujanabisri@gmail.com>
2025-06-15 23:54:32 +0700

    added yaml-cpp

--- external/llama.cpp/common/build-info.cpp
+++ external/llama.cpp/common/build-info.cpp
@@ -1,4 +1,4 @@
-int LLAMA_BUILD_NUMBER = 24;
-char const *LLAMA_COMMIT = "3ce6d33";
+int LLAMA_BUILD_NUMBER = 25;
+char const *LLAMA_COMMIT = "43655cf";
 char const *LLAMA_COMPILER = "MSVC 19.43.34810.0";
 char const *LLAMA_BUILD_TARGET = "x64";
