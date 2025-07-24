8cb8d67e89981c12317a2e120fc87386a24c3294
Evintkoo <99308658+Evintkoo@users.noreply.github.com>
2025-07-24 11:59:45 +0800

    update llama build info

--- external/llama.cpp/common/build-info.cpp
+++ external/llama.cpp/common/build-info.cpp
@@ -1,4 +1,4 @@
-int LLAMA_BUILD_NUMBER = 180;
-char const *LLAMA_COMMIT = "f4a30d53";
+int LLAMA_BUILD_NUMBER = 181;
+char const *LLAMA_COMMIT = "b8474397";
 char const *LLAMA_COMPILER = "MSVC 19.41.34120.0";
 char const *LLAMA_BUILD_TARGET = "x64";
